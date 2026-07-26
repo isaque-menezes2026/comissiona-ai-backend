2575f1386f651223e165cce8845a993f19a38db9
isaque-menezes2026 <isaque10@gmail.com>
2026-07-26 07:38:58 -0300

    feat: controller de clientes passa usuario logado para o service (escopo por vendedor/parceiro)

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param, Query, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Body, Param, Query, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CustomersService } from './customers.service';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
@@ -10,9 +10,8 @@ import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 export class CustomersController {
   constructor(private svc: CustomersService) {}
 
-  @Get() findAll(@Request() req, @Query() q: any) { return this.svc.findAll(req.user.tenantId, q); }
-  @Get(':id') findOne(@Request() req, @Param('id') id: string) { return this.svc.findOne(req.user.tenantId, id); }
+  @Get() findAll(@Request() req, @Query() q: any) { return this.svc.findAll(req.user.tenantId, q, req.user); }
+  @Get(':id') findOne(@Request() req, @Param('id') id: string) { return this.svc.findOne(req.user.tenantId, id, req.user); }
   @Post() create(@Request() req, @Body() body: any) { return this.svc.create(req.user.tenantId, body); }
   @Patch(':id') update(@Request() req, @Param('id') id: string, @Body() body: any) { return this.svc.update(req.user.tenantId, id, body); }
-  @Delete(':id') remove(@Request() req, @Param('id') id: string) { return this.svc.remove(req.user.tenantId, id); }
 }
